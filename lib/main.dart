58e60ca08354dcf0dd2a05fea95b0d5ad143e027
Kenta Aikawa <kalupas226@gmail.com>
2020-04-23 12:14:09 +0900

    add firebase asociation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:pickthree/src/models/type_data.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_admob/firebase_admob.dart';
 import 'src/my_app.dart';
 
 void main() {
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,11 +1,32 @@
+import 'dart:io';
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import '../models/type_data.dart';
 import 'package:pickthree/src/screens/pick_add_screen.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const id = 'home_screen';
 
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseAdMob.instance.initialize(appId: getAppId());
+    myBanner
+      ..load()
+      ..show(
+        // ボトムからのオフセットで表示位置を決定
+        anchorOffset: 0.0,
+        anchorType: AnchorType.bottom,
+      );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,3 +58,35 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+String getAppId() {
+  if (Platform.isIOS) {
+    return 'ca-app-pub-3940256099942544~1458002511';
+  } else if (Platform.isAndroid) {
+    return 'ca-app-pub-3940256099942544~3347511713';
+  }
+  return null;
+}
+
+// 広告ターゲット
+MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+  keywords: <String>['flutterio', 'beautiful apps'],
+  contentUrl: 'https://flutter.io',
+  birthday: DateTime.now(),
+  childDirected: false,
+  designedForFamilies: false,
+  gender: MobileAdGender.male, // or female, unknown
+  testDevices: <String>[], // Android emulators are considered test devices
+);
+
+BannerAd myBanner = BannerAd(
+  // テスト用のIDを使用
+  // リリース時にはIDを置き換える必要あり
+  adUnitId: BannerAd.testAdUnitId,
+  size: AdSize.smartBanner,
+  targetingInfo: targetingInfo,
+  listener: (MobileAdEvent event) {
+    // 広告の読み込みが完了
+    print("BannerAd event is $event");
+  },
+);
